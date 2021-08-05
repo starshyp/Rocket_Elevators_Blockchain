cf2469cc7c2f01c19f18a847e9f9eb8a58f8f3ff
fexon145 <eminem_20_mai@hotmail.com>
2021-08-05 10:15:02 -0400

    First commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+const MaterialProvider = artifacts.require("MaterialProvider");
+
 var RocketToken = artifacts.require("RocketToken");
 
 module.exports = function(deployer) {
   deployer.deploy(RocketToken, 'RocketToken', 'RT');
+  deployer.deploy(MaterialProvider);
 };
